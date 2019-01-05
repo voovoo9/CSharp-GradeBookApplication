60e76d3f9c5ff821f07963781b85490fe263a731
voovoo9 <voovoo9@gmail.com>
2019-01-05 15:45:48 +0100

    Completed the module

--- GradeBook/Enums/GradeBookType.cs
+++ GradeBook/Enums/GradeBookType.cs
@@ -1,12 +1,10 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace GradeBook.Enums
 {
-    public enum GradeBookType
+    class GradeBookType
     {
-        Standard,
-        Ranked,
-        ESNU,
-        OneToFour,
-        SixPoint
     }
 }
